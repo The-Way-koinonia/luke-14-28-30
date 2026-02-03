72659cae5ca4bf0e30910a52a3ed5d7b8fa3cb77
Colin <colinmontes94@gmail.com>
2026-02-03 00:40:05 -0500

    mobile failure

--- apps/mobile/babel.config.js
+++ apps/mobile/babel.config.js
@@ -1,17 +1,7 @@
-const path = require('path');
-
-// Ensure ABS_APP_ROOT is available for the inline transformation
-process.env.EXPO_ROUTER_ABS_APP_ROOT = path.resolve(__dirname, 'app');
-
 module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: [
-      'react-native-reanimated/plugin',
-      ['transform-inline-environment-variables', {
-        include: ['EXPO_ROUTER_APP_ROOT', 'EXPO_ROUTER_IMPORT_MODE', 'EXPO_ROUTER_ABS_APP_ROOT']
-      }]
-    ],
+    plugins: ['react-native-reanimated/plugin'],
   };
 };
--- apps/mobile/metro.config.js
+++ apps/mobile/metro.config.js
@@ -1,12 +1,9 @@
-process.env.EXPO_ROUTER_APP_ROOT = 'app';
-
 const { getDefaultConfig } = require('expo/metro-config');
 const path = require('path');
 
 const projectRoot = __dirname;
 const workspaceRoot = path.resolve(projectRoot, '../..');
 
-/** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(projectRoot);
 
 config.watchFolders = [workspaceRoot];
@@ -14,12 +11,12 @@ config.resolver.nodeModulesPaths = [
   path.resolve(projectRoot, 'node_modules'),
   path.resolve(workspaceRoot, 'node_modules'),
 ];
+config.resolver.disableHierarchicalLookup = true;
 
 config.resolver.extraNodeModules = {
   'react': path.resolve(workspaceRoot, 'node_modules/react'),
   'react-native': path.resolve(workspaceRoot, 'node_modules/react-native'),
   'expo': path.resolve(workspaceRoot, 'node_modules/expo'),
-  'react-dom': path.resolve(workspaceRoot, 'node_modules/react-dom'),
 };
 
 config.resolver.assetExts.push('db');
